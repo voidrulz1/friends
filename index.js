ab05e1ebe944695a5fab419457f286934a05b2ae
voidrulz1 <131723217+voidrulz1@users.noreply.github.com>
2023-04-25 09:49:35 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+
+
+
+const keepAlive = require('./server');
+const Monitor = require('ping-monitor');
+
+
+
+keepAlive();
+
+const monitor = new Monitor({
+  website: 'https://friends.voidrulz.repl.co',
+  title: 'NAME',
+  interval: 3
+});
+
+monitor.on('up', (res) => {
+  var today = new Date();
+  var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date + ' ' + time;
+  //console.log(`${res.website} its on. - ${dateTime}`);
+})
+monitor.on('down', (res) => {
+  var today = new Date();
+  var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date + ' ' + time;
+  console.log(`${res.website} it has died - ${res.statusMessage} - ${dateTime}`);
+})
+monitor.on('stop', (website) => {
+  var today = new Date();
+  var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date + ' ' + time;
+  console.log(`${website} has stopped. - ${dateTime}`)
+});
+monitor.on('error', (error) => {
+  var today = new Date();
+  var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date + ' ' + time;
+  console.log(error + " - " + dateTime)
+});
+
